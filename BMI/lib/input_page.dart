c9ad2c837d7b1d83eea042765425fa6dbe38cbe7
AsadBashir198 <asadbashir19832@gmail.com>
2022-04-12 22:45:58 -0700

    Use of refactored code
    used refactor to short the code in container.

--- BMI/lib/input_page.dart
+++ BMI/lib/input_page.dart
@@ -1,4 +1,5 @@
-
+import 'package:flutter/cupertino.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter/material.dart';
 
 class inputpage extends StatefulWidget {
@@ -21,9 +22,18 @@ class _inputpageState extends State<inputpage> {
             children: [
               Expanded(child:RepeatContainerCode(
                 colors:Color(0xFF1D1E33),
+                cardwidget: repeatTextandIcon(
+                  iconData: FontAwesomeIcons.male,
+                  label: 'Male',
+                ),
+
               ),),
               Expanded(child:RepeatContainerCode(
                 colors:Color(0xFF1D1E33),
+                cardwidget: repeatTextandIcon(
+                  iconData: FontAwesomeIcons.female,
+                  label: 'Female',
+                ),
               ),),
             ],
 
@@ -32,7 +42,11 @@ class _inputpageState extends State<inputpage> {
           Expanded(child:Row(
             children: [
               Expanded(child:RepeatContainerCode(
-                colors:Colors.purpleAccent,
+               colors:Color(0xFF1D1E33),
+                cardwidget: repeatTextandIcon(
+                  iconData: FontAwesomeIcons.female,
+                  label: 'Female',
+                ),
               ),),
             ],
           ),),
@@ -40,9 +54,17 @@ class _inputpageState extends State<inputpage> {
           Expanded(child:Row(children: [
             Expanded(child:RepeatContainerCode(
               colors:Color(0xFF1D1E33),
+              cardwidget: repeatTextandIcon(
+                iconData: FontAwesomeIcons.male,
+                label: 'Male',
+              ),
             ),),
             Expanded(child:RepeatContainerCode(
               colors:Color(0xFF1D1E33),
+              cardwidget: repeatTextandIcon(
+                iconData: FontAwesomeIcons.male,
+                label: 'Male',
+              ),
             ),),
           ],
 
@@ -54,14 +76,47 @@ class _inputpageState extends State<inputpage> {
   }
 }
 
+class repeatTextandIcon extends StatelessWidget {
+  repeatTextandIcon({required this.iconData,required this.label});
+ final  IconData iconData;
+ final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          iconData ,
+          size: 80.0,
+
+        ),
+         SizedBox(
+           height: 20.0,
+           width: 21.0,
+         ),
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 20.0,
+            color: Color(0xFF8D8E98),
+          ),
+        )
+      ],
+    );
+  }
+}
+
 class RepeatContainerCode extends StatelessWidget {
-  RepeatContainerCode({required this.colors});
-  Color colors;
+  RepeatContainerCode({required this.colors,required this.cardwidget});
+  final Color colors;
+  final  Widget cardwidget;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15.0),
+      child: cardwidget,
       decoration: BoxDecoration(
         color:colors,
         borderRadius: BorderRadius.circular(10.0)
