d7859dfd197e6696b9a306259e84e1a20d38de3f
AsadBashir198 <asadbashir19832@gmail.com>
2022-07-15 10:34:25 +0500

    first commit

---
+++ fees_management_app/lib/login.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:fees_management_app/autication.dart';
+import 'package:fees_management_app/signup.dart';
+
+
+
+class abc extends StatefulWidget {
+  @override
+  State<abc> createState() => _abcState();
+}
+
+class _abcState extends State<abc> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      home: Scaffold(
+        body: Container(
+
+        decoration: BoxDecoration(
+        image: DecorationImage(
+        image: AssetImage('assets/sign.png'), fit: BoxFit.cover),),
+          child: Padding(
+              padding: const EdgeInsets.only(top:170),
+              child: ListView(
+                children: <Widget>[
+                  Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.all(10),
+                      child: const Text(
+                        'Job Finder App',
+                        style: TextStyle(
+                            color: Colors.blue,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 30),
+                      )),
+                  Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.all(10),
+                      child: const Text(
+                        'Sign in',
+                        style: TextStyle(fontSize: 20),
+                      )),
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    child: TextField(
+                      controller: nameController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Email',
+                      ),
+                    ),
+
+                  ),
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                    child: TextField(
+                      obscureText: true,
+                      controller: passwordController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Password',
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      //forgot password screen
+                    },
+                    child: const Text('Forgot Password',),
+                  ),
+                  Container(
+                      height: 50,
+                      padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                      child: ElevatedButton(
+                        child: const Text('Login'),
+                        onPressed: () {
+                          AuthenticationHelper()
+                              .signIn(email: nameController.text, password: passwordController.text)
+                              .then((result) {
+                            if (result == null) {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => Homepage()),
+                              );
+                            } else {
+                              Scaffold.of(context).showSnackBar(SnackBar(
+                                content: Text(
+                                  result,
+                                  style: TextStyle(fontSize: 16),
+                                ),
+                              ));
+                            }
+                          });
+                        },
+                      )
+                  ),
+                  Row(
+                    children: <Widget>[
+                      const Text('Does not have account?'),
+                      TextButton(
+                        child: const Text(
+                          'Sign up',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => MyStatefulWidget()),
+                          );
+                          //signup screen
+                        },
+                      )
+                    ],
+                    mainAxisAlignment: MainAxisAlignment.center,
+                  ),
+                ],
+              )),
+        ),
+      ),
+    );
+  }
+}
