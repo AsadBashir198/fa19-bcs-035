56c15c45aa068cd3df4cfbd95e1b2d6c4db00e85
AsadBashir198 <100112110+AsadBashir198@users.noreply.github.com>
2023-05-25 11:45:27 -0700

    update cv
    modification in cv module

--- aris_flutterjobportal-master/lib/cv-maker/cv.dart
+++ aris_flutterjobportal-master/lib/cv-maker/cv.dart
@@ -1,8 +1,11 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:pdf/pdf.dart';
 import 'package:path_provider/path_provider.dart';
+
+import '../jobseeker/home_page.dart';
 
 
 
@@ -10,10 +13,15 @@ class resume1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'PDF App',
+      debugShowCheckedModeBanner: false,
+      title: 'Make YOur CV',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+
+          appBarTheme: AppBarTheme(
+            centerTitle: true,
+            color: const Color(0xFF031047),
+          )),
+
       home: cv_home(),
     );
   }
@@ -29,9 +37,20 @@ class _cv_homeState extends State<cv_home> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController phoneController = TextEditingController();
 
+  final TextEditingController degreecontroller = TextEditingController();
+  final TextEditingController institutecontroller = TextEditingController();
+  final TextEditingController malecontroller= TextEditingController();
+  final TextEditingController datedcontroller = TextEditingController();
+  final TextEditingController hobbiescontroller = TextEditingController();
+  final TextEditingController achievementcontroller = TextEditingController();
+  final TextEditingController workexperiencecontroller = TextEditingController();
+  final TextEditingController skillscontroller = TextEditingController();
 
 
-  
+
+
+
+
   final TextEditingController fileNameController = TextEditingController();
   String resultMessage = '';
 
@@ -39,38 +58,104 @@ class _cv_homeState extends State<cv_home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('PDF App'),
+        title: Text('CV Maker'),
+        leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) =>  Homepage()),
+              );
+            }
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: nameController,
-              decoration: InputDecoration(labelText: 'Name'),
-            ),
-            TextField(
-              controller: emailController,
-              decoration: InputDecoration(labelText: 'Email'),
-            ),
-            TextField(
-              controller: phoneController,
-              decoration: InputDecoration(labelText: 'Phone'),
-            ),
-            TextField(
-              controller: fileNameController,
-              decoration: InputDecoration(labelText: 'File Name'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                generatePDF();
-              },
-              child: Text('Generate PDF'),
-            ),
-            SizedBox(height: 20),
-            Text(resultMessage),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text(
+                'Personal Details',
+                style: TextStyle(fontSize: 20),
+              ),
+              TextField(
+                controller: nameController,
+                decoration: InputDecoration(labelText: 'Name'),
+              ),
+              TextField(
+                controller: emailController,
+                decoration: InputDecoration(labelText: 'Email'),
+              ),
+              TextField(
+                controller: phoneController,
+                decoration: InputDecoration(labelText: 'Phone'),
+              ),
+              TextField(
+                controller: datedcontroller,
+                decoration: InputDecoration(labelText: 'Date of Birth'),
+              ),
+              TextField(
+                controller: malecontroller,
+                decoration: InputDecoration(labelText: 'Gender'),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Educational Details',
+                style: TextStyle(fontSize: 20),
+              ),
+              TextField(
+                controller: degreecontroller,
+                decoration: InputDecoration(labelText: 'Degree'),
+              ),
+              TextField(
+                controller: institutecontroller,
+                decoration: InputDecoration(labelText: 'Institute'),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Other Informations',
+                style: TextStyle(fontSize: 20),
+              ),
+
+              TextField(
+                decoration: InputDecoration(labelText: 'Skills'),
+                controller: skillscontroller,
+                keyboardType: TextInputType.multiline,
+                maxLines: null, // <-- SEE HERE
+              ),
+              SizedBox(height: 20),
+              TextField(
+                decoration: InputDecoration(labelText: 'Achievements'),
+                controller: achievementcontroller,
+                keyboardType: TextInputType.multiline,
+                maxLines: null, // <-- SEE HERE
+              ),
+              SizedBox(height: 20),
+              TextField(
+                decoration: InputDecoration(labelText: 'Work Experience'),
+                controller: workexperiencecontroller,
+                keyboardType: TextInputType.multiline,
+                maxLines: null, // <-- SEE HERE
+              ),
+
+              TextField(
+                controller: fileNameController,
+                decoration: InputDecoration(labelText: 'CV_Title'),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  generatePDF();
+                },
+                child: Text('Generate PDF'),
+              ),
+              SizedBox(height: 20),
+              Text(resultMessage),
+            ],
+          ),
         ),
       ),
     );
@@ -82,6 +167,20 @@ class _cv_homeState extends State<cv_home> {
     final name = nameController.text;
     final email = emailController.text;
     final phone = phoneController.text;
+    final deg = degreecontroller.text;
+    final insti = institutecontroller.text;
+    final mal = malecontroller.text;
+    final dat = datedcontroller.text;
+    final hobie = hobbiescontroller.text;
+    final achiev = achievementcontroller.text;
+    final w_exp = workexperiencecontroller.text;
+    final skill = skillscontroller.text;
+
+
+
+
+
+
     final fileName = fileNameController.text.isNotEmpty ? fileNameController.text : 'user_details';
 
     pdf.addPage(
@@ -92,11 +191,44 @@ class _cv_homeState extends State<cv_home> {
               mainAxisAlignment: pw.MainAxisAlignment.center,
               crossAxisAlignment: pw.CrossAxisAlignment.start,
               children: [
-                pw.Text('Name: $name', style: pw.TextStyle(fontSize: 18)),
-                pw.SizedBox(height: 10),
-                pw.Text('Email: $email', style: pw.TextStyle(fontSize: 18)),
-                pw.SizedBox(height: 10),
-                pw.Text('Phone: $phone', style: pw.TextStyle(fontSize: 18)),
+                pw.Text('Personal Details',style:pw.TextStyle(fontSize: 30)),
+
+                pw.Text('Name: $name',textAlign: pw.TextAlign.left, style: pw.TextStyle(fontSize: 20)),
+                pw.SizedBox(height: 3),
+                pw.Text('Email: $email',textAlign: pw.TextAlign.left, style: pw.TextStyle(fontSize: 20)),
+                pw.SizedBox(height: 3),
+                pw.Text('Gender: $mal', textAlign: pw.TextAlign.left,style: pw.TextStyle(fontSize: 20)),
+                pw.SizedBox(height: 3),
+                pw.Text('Phone: $phone',textAlign: pw.TextAlign.left, style: pw.TextStyle(fontSize: 20)),
+                pw.SizedBox(height: 3),
+                pw.SizedBox(height: 3),
+                pw.Text('Date of Birth: $dat',textAlign: pw.TextAlign.left, style: pw.TextStyle(fontSize: 20)),
+
+                pw.Text('Education Details',style:pw.TextStyle(fontSize: 30)),
+
+                pw.Text('Degree: $deg',textAlign: pw.TextAlign.left, style: pw.TextStyle(fontSize: 20)),
+                pw.SizedBox(height: 3),
+                pw.Text('Institute: $insti', textAlign: pw.TextAlign.left,style: pw.TextStyle(fontSize: 20)),
+
+
+                pw.Text('Achievements ',style:pw.TextStyle(fontSize: 30)),
+                pw.SizedBox(height: 3),
+                pw.Text('Achievements: $achiev',textAlign: pw.TextAlign.left, style: pw.TextStyle(fontSize: 20)),
+
+
+                pw.Text('Work Experience',style:pw.TextStyle(fontSize: 30)),
+                pw.SizedBox(height: 3),
+                pw.Text('Work_Experience: $w_exp', textAlign: pw.TextAlign.left,style: pw.TextStyle(fontSize: 20)),
+
+                pw.Text('Hobbies',style:pw.TextStyle(fontSize: 30)),
+                pw.SizedBox(height: 3),
+                pw.Text('Hobbies: $hobie', textAlign: pw.TextAlign.left,style: pw.TextStyle(fontSize: 20)),
+
+                pw.Text('skills',style:pw.TextStyle(fontSize: 30)),
+                pw.SizedBox(height: 3),
+                pw.Text('skills: $skill', textAlign: pw.TextAlign.left,style: pw.TextStyle(fontSize: 20,)),
+
+
               ],
             ),
           );
